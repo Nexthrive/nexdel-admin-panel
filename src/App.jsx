8e23ddbc832f0badbdd0409706f0889e9c53a53e
Edgar <edgarmasstudio@gmail.com>
2024-04-21 07:58:16 +0700

    Working login
    Added:
    - Browser Router
    - App.jsx, Routes ( Temporary)
    - Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.scss";
 import Login from "./pages/login/index";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/index";
 
 function App() {
-  return <Login />;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
