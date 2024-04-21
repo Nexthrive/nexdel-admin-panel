8eb4ac5a26e2a2b08b2e57f559b2b6fd8dc1d2c1
aldiyusronzhar <aldiyusronzhar@gmail.com>
2024-04-21 14:09:36 +0700

    Improve Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/index";
-import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/index";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
